fca787a9298d0f518a5d6d19eb1a642df970fb93
sementeviva <vogelthiago55@gmail.com>
2025-05-22 10:52:27 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-import psycopg2 import os
+import psycopg2
+import os
 
 def get_connection(): return psycopg2.connect( host=os.environ.get("SUPABASE_HOST"), dbname=os.environ.get("SUPABASE_DBNAME"), user=os.environ.get("SUPABASE_USER"), password=os.environ.get("SUPABASE_PASSWORD"), port=os.environ.get("SUPABASE_PORT", 5432) )
 
