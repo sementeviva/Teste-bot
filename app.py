f1274226b4903601f3649ac26c92938873e28e70
sementeviva <vogelthiago55@gmail.com>
2025-05-22 13:37:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -124,5 +124,11 @@ def whatsapp_webhook():
 
     return "OK", 200
 
+from flask import render_template
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
