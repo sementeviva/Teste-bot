a9a46766d4d97b1694bfe13d0a8241e0b0818aff
sementeviva <vogelthiago55@gmail.com>
2025-05-31 12:39:26 -0300

    Update forms.py

--- routes/forms.py
+++ routes/forms.py
@@ -2,6 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from wtforms.validators import DataRequired
 
-class UploadForm(FlaskForm):
-    csv_file = FileField('Arquivo CSV ou Excel', validators=[DataRequired()])
+class UploadCSVForm(FlaskForm):
+    file = FileField('Arquivo CSV', validators=[DataRequired()])
     submit = SubmitField('Enviar')
