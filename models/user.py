1c7fedb38e4c3a9821856318b58b7bf44ad95a6f
sementeviva <vogelthiago55@gmail.com>
2025-06-18 09:28:35 -0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,13 @@
 from flask_login import UserMixin
 
 class User(UserMixin):
-    def __init__(self, id, nome, email, conta_id):
+    def __init__(self, id, nome, email, conta_id, is_admin=False):
         self.id = id
         self.nome = nome
         self.email = email
         self.conta_id = conta_id
+        self.is_admin = is_admin
 
     # O Flask-Login usa este método para obter o ID do utilizador
     def get_id(self):
         return str(self.id)
-
